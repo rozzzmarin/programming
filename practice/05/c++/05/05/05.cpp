631160549dd287a3bf86e7c684118e9a8586d167
rozzzmarin <70970146+rozzzmarin@users.noreply.github.com>
2020-09-20 21:22:24 +0300

    Update 05.cpp

--- practice/05/c++/05/05/05.cpp
+++ practice/05/c++/05/05/05.cpp
@@ -6,6 +6,6 @@ int main() {
     double a = 9.8;
     double x, x0, v0, t;
     cin >> x0 >> v0 >> t;
-    x = x0 + (v0 * t) - (a * t * t / 2);
+    x = x0 + (v0 * t) + (a * t * t / 2);
     cout << x;
 }
