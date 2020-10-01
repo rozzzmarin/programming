d50aeba2a9a941e8254b45c6bc0a4fde23aaba3c
rozzzmarin <70970146+rozzzmarin@users.noreply.github.com>
2020-10-01 23:42:42 +0300

    Update 05.cpp

--- practice/05/c++/05/05/05.cpp
+++ practice/05/c++/05/05/05.cpp
@@ -1,11 +1,12 @@
-﻿#include <iostream>
+#include <iostream>
 
 int main() {
     using namespace std;
-
+    setlocale(LC_ALL, "Russian");
     double a = 9.8;
-    double x, x0, v0, t;
-    cin >> x0 >> v0 >> t;
+    double x, x0, v0, t, d;
+    cout << "Введите значения x0, v0, t: "; cin >> x0 >> v0 >> t;
     x = x0 + (v0 * t) - (a * t * t / 2);
-    cout << x;
+    d = abs(x - x0);
+    cout << "Объект преодолеет расстояние, равное " << d;
 }
