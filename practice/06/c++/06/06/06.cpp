9277406250f52458b0c86ad1e05d1fe28d3bc66e
Rema <zhrrsmrn@gmail.com>
2020-10-21 22:54:25 +0300

    new tasks added

---
+++ practice/06/c++/06/06/06.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    using namespace std;
+    double a, b, c, d, x1, x2;
+
+    while (true) {
+        cout << "а = "; cin >> a;
+        cout << "b = "; cin >> b;
+        cout << "c = "; cin >> c;
+        if (a == 0) {
+            if (b != 0 && c != 0) {
+                x1 = -(c / b); cout << "x = " << x1;
+            }
+            else if (b != 0 && c == 0) {
+                x1 = (0 / b); cout << "x = " << x1;
+            }
+            else {
+                if (b == 0 && c != 0) cout << "Нет корней.";
+                if (b == 0 && c == 0) cout << "Любое х является корнем.";
+            }
+        }
+        if (a != 0) {
+            if (b != 0 && c != 0) {
+                d = (b * b) - (4 * a * c);
+                if (d > 0) {
+                    x1 = (-b + sqrt(d)) / (2 * a); cout << "x1 = " << x1;
+                    x2 = (-b - sqrt(d)) / (2 * a); cout << "x2 = " << x2;
+                }
+                else {
+                    if (d == 0) {
+                        x1 = -b / (2 * a); cout << "x = " << x1;
+                    }
+                    if (d < 0) { cout << "Уравнение имеет не вещественные корни."; }
+                }
+            }
+            else if (b != 0 && c == 0) {
+                x1 = 0; x2 = -b / a;
+                cout << "x1 = " << x1 << ", x2 = " << x2;
+            }
+            else if (b == 0) {
+                if (c == 0) {
+                    x1 = sqrt(0 / a);  cout << "x =" << x1;
+                }
+                if (c != 0) {
+                    x1 = sqrt(c); x2 = -(sqrt(c));
+                    cout << "x1 = " << x1 << ", x2 = " << x2;
+                }
+            }
+        }
+    }
+}
---
+++ practice/07/c++/07/07/07.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+int main() {
+    using namespace std;
+    setlocale(LC_ALL, "Russian");
+    double S, p, a, b, c, xa, ya, xb, yb, xc, yc, method;
+    while (cout << "Выберите способ ввода параметров треугольника:" << endl << "1. Длины сторон" << endl << "2. Координаты вершин" << endl && !(cin >> method) && !(method == 1) && !(method == 1)) {
+        cin.clear();
+        cout << "Входные данные неверны. Попробуйте ещё раз.";
+        getline
+    }
+    if (method == 1) {
+        cout << "Введите длину стороны а: "; cin >> a; cout << "Введите длину стороны b: "; cin >> b; cout << "Введите длину стороны c: "; cin >> c;
+        p = (a + b + c) / 2;
+        S = sqrt(p * (p - a) * (p - b) * (p - c));
+        cout << "S = " << S;
+    }
+    if (method == 2) {
+        cout << "Введите координаты веришны А (через пробел): "; cin >> xa >> ya; cout << "Введите координаты вершины B (через пробел): "; cin >> xb >> yb; cout << "Введите координаты веришн С (через пробел): "; cin >> xc >> yc;
+        a = sqrt((pow((xa - ya), 2) - (pow((xb - yb), 2))));
+        b = sqrt((pow((xb - yb), 2) - (pow((xc - yc), 2))));
+        c = sqrt((pow((xc - yc), 2) - (pow((xa - ya), 2))));
+        p = (a + b + c) / 2;
+        S = sqrt(p * (p - a) * (p - b) * (p - c));
+        cout << "S = " << S;
+    }
+}
