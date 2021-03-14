dfd4c71bf0d58217887e7a40e61f3e0b5f22be76
Rema <zhrrsmrn@gmail.com>
2021-03-14 23:58:59 +0300

    33, 34

---
+++ practice/33/c++/33try/33try.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+
+int* create(int le, int f_e = 0, int st = 0) {
+    int* array = new int[le];
+    int steppy = 0;
+    for (int i = 0; i < le; i++) {
+
+        array[i] = f_e + steppy;
+        steppy += st;
+    }
+    /*for (int i = 0; i < le; i++) {
+
+        array[i] = f_e + st*i;
+       // steppy += st;
+    }
+
+    array[0] = f_e;
+    for (int i = 1; i < le; i++) {
+
+        array[i] = array[i-1] + st;
+
+    }*/
+    return array;
+}
+void print(int* array, int le){
+    for (int i = 0; i < le; i++) {
+        std::cout << array[i] << ' ';
+    }
+}
+
+int* sort(int* array, int le) {
+    for (int i = 1; i < le; i++) {
+        for (int j = i; j > 0 && array[j - 1] > array[j]; j--) {
+            int temp = array[j - 1];
+            array[j - 1] = array[j];
+            array[j] = temp;
+        }
+    }
+    return array;
+}
+
+int main() {
+    int length, first_elt = 0, step = 0;
+    std::cout << "plz input array length, the first element and the step!\n";
+    std::cin >> length >> first_elt >> step; 
+    
+    int* a = create(length, first_elt, step);
+    print(sort(a, length), length);
+    delete[] a;
+}
---
+++ practice/34/c++/34try/34try.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+void create(int *what, int le, int f_e = 0, int st = 0) {
+    int* array = new int[le];
+    int steppy = 0;
+    for (int i = 0; i < le; i++) {
+
+        array[i] = f_e + steppy;
+        steppy += st;
+    }
+}
+
+
+int main()
+{
+    int length, first_elt = 0, step = 0;
+    create()
+}
